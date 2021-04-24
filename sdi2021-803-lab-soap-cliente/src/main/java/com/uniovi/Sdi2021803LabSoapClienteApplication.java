8f14a9b022400ea4c3458d9b6fd72b7d491a5776
uo269736 <uo269736@uniovi.es>
2021-04-24 13:00:43 +0200

    SDI-2021-803-SW-11.2- Servicio SOAP Cliente

---
+++ sdi2021-803-lab-soap-cliente/src/main/java/com/uniovi/Sdi2021803LabSoapClienteApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021803LabSoapClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021803LabSoapClienteApplication.class, args);
+	}
+
+}
